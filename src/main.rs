efd0dbb67be10efccab0da05535ff1c4fc3dfecf
angelsflyinhell <angelsflyinhell@namespace.media>
2023-08-01 17:27:30 +0200

    feat: add env_logger

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
-use std::{env, time::Duration};
-
 use events::handler::Handler;
+use log::{error, info};
 use serenity::{client::bridge::gateway::GatewayIntents, Client};
+use std::{env, time::Duration};
 use tokio::time::sleep;
 
+mod commands;
 mod events;
 
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().expect("Failed to read .env file");
+    env_logger::init();
 
     // client initialization
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
@@ -36,20 +38,20 @@ async fn main() {
 
             for (id, runner) in shard_runners.iter() {
                 if runner.latency.is_some() {
-                    println!(
-                        "Shard ID {} is {:?} ({:?})",
+                    info!(
+                        "Shard {} is {:?} ({:?} latency)",
                         id.0 + 1,
                         runner.stage,
                         runner.latency.unwrap()
                     );
                 } else {
-                    println!("Shard ID {} is {:?}", id.0 + 1, runner.stage);
+                    info!("Shard {} is {:?}", id.0 + 1, runner.stage);
                 }
             }
         }
     });
 
     if let Err(why) = client.start_autosharded().await {
-        println!("Client error: {:?}", why);
+        error!("Client error: {:?}", why);
     }
 }
